936abfdf7ebc83fe7574307b56bb13da0b73f148
coldreason <coldreason@korea.ac.kr>
2022-09-27 19:41:52 +0900

    change to trenbe

--- lib/main.dart
+++ lib/main.dart
@@ -5,250 +5,307 @@ void main() {
   runApp(Home());
 }
 
-List<Map<String, dynamic>> datas = [
-  {
-    'pid': '0',
-    'location': '신공학관',
-    'image': 'assets/images/ara-1.jpg',
-    'title': '신발',
-    'likes': '0'
-  },
-  {
-    'pid': '0',
-    'location': '제2 공학관',
-    'image': 'assets/images/ara-2.jpg',
-    'title': '고기',
-    'likes': '3'
-  },
-  {
-    'pid': '0',
-    'location': '신공학관',
-    'image': 'assets/images/ara-3.jpg',
-    'title': '치약',
-    'likes': '2'
-  },
-  {
-    'pid': '0',
-    'location': '신공학관',
-    'image': 'assets/images/ara-4.jpg',
-    'title': '맥북',
-    'likes': '0'
-  },
-  {
-    'pid': '0',
-    'location': '전동드릴',
-    'image': 'assets/images/ara-5.jpg',
-    'title': '신발',
-    'likes': '0'
-  },
-  {
-    'pid': '0',
-    'location': '신공학관',
-    'image': 'assets/images/ara-6.jpg',
-    'title': '핸드폰',
-    'likes': '0'
-  },
-  {
-    'pid': '0',
-    'location': '신공학관',
-    'image': 'assets/images/ara-7.jpg',
-    'title': '신발',
-    'likes': '0'
-  }
-];
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: TrenbeAppbar(),
+          body: TrenbeBody(),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _selected,
+            selectedFontSize: 12.0,
+            showSelectedLabels: true,
+            selectedItemColor: Colors.purple,
+            selectedLabelStyle: TextStyle(color: Colors.black),
+            onTap: (index) {
+              setState(() {
+                _selected = index;
+              });
+            },
+            items: [
+              _bottomNavigationBarItem(
+                  Icons.category_outlined, Icons.category, '카테고리'),
+              _bottomNavigationBarItem(
+                  Icons.gif_box_outlined, Icons.gif_box, '동네생활'),
+              _bottomNavigationBarItem(Icons.home_outlined, Icons.home, '홈'),
+              _bottomNavigationBarItem(
+                  Icons.shopping_bag_outlined, Icons.shopping_bag, '뉴시즌'),
+              _bottomNavigationBarItem(
+                  Icons.account_circle_outlined, Icons.account_circle, '마이'),
+            ],
+          ),
+          floatingActionButton: FloatingButtonCustom()),
+    );
+  }
+}
+
+class TrenbeBody extends StatefulWidget {
+  const TrenbeBody({Key? key}) : super(key: key);
+
+  @override
+  State<TrenbeBody> createState() => _TrenbeBodyState();
+}
+
+class _TrenbeBodyState extends State<TrenbeBody> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 6, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(
+        children: [
+          Expanded(child: TextField()),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            iconSize: 22,
+          )
+        ],
+      ),
+      SizedBox(height: 20,),
+      Container(
+        child: TabBar(
+          tabs: [
+            TabBarTab(name: '홈'),
+            TabBarTab(name: '기획전'),
+            TabBarTab(name: '랭킹'),
+            TabBarTab(name: '브랜드'),
+            TabBarTab(name: 'IT:EM'),
+            TabBarTab(name: '매거진'),
+          ],
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey,
+          controller: _tabController,
+          indicatorWeight: 0.1,
+        ),
+      ),
+      SizedBox(height: 20,),
+      Expanded(
+        child: TabBarView(
+          controller: _tabController,
+          children: [
+            Container(
+                color: Colors.white,
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Container(
+                        width: double.infinity,
+                        height: 200,
+                        child: Image.asset(
+                          'assets/images/home_img_1.jpg',
+                          fit: BoxFit.fill,
+                        )),
+                    SizedBox(height: 30,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                      children: [
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                        MainButton(imagesrc: 'assets/images/images.jpg',text: '여성',),
+                      ],
+                    ),
+                  ],
+                )),
+            Container(
+              color: Colors.green[200],
+              alignment: Alignment.center,
+              child: Text(
+                'Tab2 View',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.red[200],
+              alignment: Alignment.center,
+              child: Text(
+                'Tab3 View',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.yellow[200],
+              alignment: Alignment.center,
+              child: Text(
+                'Tab4 View',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.purple[200],
+              alignment: Alignment.center,
+              child: Text(
+                'Tab5 View',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.cyan[200],
+              alignment: Alignment.center,
+              child: Text(
+                'Tab6 View',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
+  }
+
+}
+
+class MainButton extends StatelessWidget {
+  const MainButton({Key? key, required this.text, required this.imagesrc}) : super(key: key);
+
+
+  final String text;
+  final String imagesrc;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+        onPressed: () {},
+        child: Column(
+          children: [
+            Container(
+                width: 50,
+                height: 50,
+                child: Image.asset(
+                  imagesrc,
+                  fit: BoxFit.fill,
+                )),
+            Text(text)
+          ],
+        ));
+  }
+}
+
+
+class TabBarTab extends StatelessWidget {
+  const TabBarTab({Key? key, required this.name}) : super(key: key);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      alignment: Alignment.center,
+      child: Text(
+        name,
+      ),
+    );
+  }
+}
+
+class TrenbeAppbar extends StatelessWidget with PreferredSizeWidget {
+  const TrenbeAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
+      title: SvgPicture.asset(
+        'assets/svg/header_logo.svg',
+        width: 100,
+      ),
+      elevation: 1.0,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            'assets/svg/heart-svgrepo-com.svg',
+            width: 22,
+            color: Colors.black,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.shopping_bag_outlined,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+    ;
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
 
 BottomNavigationBarItem _bottomNavigationBarItem(
-    String iconName, String label) {
+    IconData iconStatic, IconData iconActive, String label) {
   return BottomNavigationBarItem(
     icon: Padding(
       padding: EdgeInsets.only(bottom: 5.0),
-      child: SvgPicture.asset('assets/svg/${iconName}_off.svg', width: 22),
+      child: Icon(iconStatic),
     ),
     activeIcon: Padding(
       padding: EdgeInsets.only(bottom: 5.0),
-      child: SvgPicture.asset('assets/svg/${iconName}_on.svg', width: 22),
+      child: Icon(iconActive),
     ),
     label: label,
   );
 }
 
-class DataListView extends StatelessWidget {
-  const DataListView({Key? key}) : super(key: key);
+class FloatingButtonCustom extends StatelessWidget {
+  const FloatingButtonCustom({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        itemBuilder: (BuildContext _context, int index) {
-          return GestureDetector(
-            onTap: () {},
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    child: Hero(
-                      tag: datas[index]['pid'],
-                      child: Image.asset(
-                        datas[index]['image'],
-                        width: 100,
-                        height: 100,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.only(left: 20.0),
-                      height: 100,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            datas[index]['title'],
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(fontSize: 15.0),
-                          ),
-                          SizedBox(height: 5.0),
-                          Text(
-                            datas[index]['location'],
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                                fontSize: 12.0,
-                                color: Colors.black.withOpacity(0.3)),
-                          ),
-                          SizedBox(height: 5.0),
-                          Text(
-                            '2000원',
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),
-                          Expanded(
-                            child: Container(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  SvgPicture.asset(
-                                    'assets/svg/heart_off.svg',
-                                    width: 13,
-                                    height: 13,
-                                  ),
-                                  SizedBox(width: 5.0),
-                                  Text(datas[index]['likes']),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-        separatorBuilder: (BuildContext _context, int index) {
-          return Container(height: 1, color: Colors.black.withOpacity(0.1));
-        },
-        itemCount: datas.length);
-  }
-}
-
-class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            automaticallyImplyLeading: false,
-            title: GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTap: () {},
-              child: PopupMenuButton<String>(
-                offset: Offset(0, 38),
-                onSelected: (String where) {},
-                itemBuilder: (BuildContext context) {
-                  return [
-                    PopupMenuItem(value: '0', child: Text('안암동')),
-                    PopupMenuItem(value: '1', child: Text('신설동')),
-                    PopupMenuItem(
-                      value: 'setting_neighborhood',
-                      child: Text('내 동네 설정하기'),
-                    ),
-                  ];
-                },
-                child: Row(
-                  children: [
-                    Text(
-                      '안암동',
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Icon(
-                      Icons.keyboard_arrow_down_rounded,
-                      color: Colors.black,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            elevation: 1.0,
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.menu,
-                  color: Colors.black,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset(
-                  'assets/svg/bell.svg',
-                  width: 22,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-          body: DataListView(),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            selectedFontSize: 12.0,
-            showSelectedLabels: true,
-            selectedItemColor: Colors.black,
-            selectedLabelStyle: TextStyle(color: Colors.black),
-            items: [
-              _bottomNavigationBarItem('home', '홈'),
-              _bottomNavigationBarItem('notes', '동네생활'),
-              _bottomNavigationBarItem('location', '내 근처'),
-              _bottomNavigationBarItem('chat', '채팅'),
-              _bottomNavigationBarItem('user', '나의 당근'),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.deepOrange,
-            child: Icon(
-              Icons.add,
-            ),
-          )),
+    return FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: Colors.white,
+      child: Icon(
+        Icons.keyboard_arrow_up_sharp,
+        size: 30,
+        color: Colors.grey,
+      ),
     );
   }
 }
